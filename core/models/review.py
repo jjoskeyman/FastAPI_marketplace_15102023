b77fbdffcedd0e993fc7f657f8e2b8ffc58b9ecd
Viktor <viktor.glikhov89@mail.ru>
2023-10-16 15:43:57 +0300

    create relationships between users and reviews

--- core/models/review.py
+++ core/models/review.py
@@ -1,7 +1,12 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Review(Base):
@@ -16,3 +21,5 @@ class Review(Base):
     user_id: Mapped[int] = mapped_column(
         ForeignKey("users.id"),
     )
+
+    user: Mapped["User"] = relationship(back_populates="reviews")
--- core/models/user.py
+++ core/models/user.py
@@ -1,10 +1,17 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .review import Review
 
 
 class User(Base):
     user_name: Mapped[str] = mapped_column(String(32), unique=True)
     first_name: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
+
+    reviews: Mapped[list["Review"]] = relationship(back_populates="user")
